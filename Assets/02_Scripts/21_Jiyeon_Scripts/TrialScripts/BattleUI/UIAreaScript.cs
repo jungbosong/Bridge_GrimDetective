e68858b0f1b22bc7e780bb003a313b89d640390f
Bosomi <she79560@naver.com>
2022-03-22 19:46:40 +0900

    [ADD] 채팅 UI 기능 공부한 내용 추가(-지연)

---
+++ Assets/02_Scripts/21_Jiyeon_Scripts/TrialScripts/BattleUI/UIAreaScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIAreaScript : MonoBehaviour
+{
+    public RectTransform AreaRect, BoxRect, TextRect;
+    public GameObject Tail;
+}
